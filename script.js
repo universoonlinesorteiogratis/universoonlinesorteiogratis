0aa3e44522b6e66f3bf4250171a9c33d7740a87e
universoonlinesorteiogratis <andreiaerayssa@gmail.com>
2025-03-07 20:57:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Configuração do Supabase
-const SUPABASE_URL = 'https://dtkonvkclvmgcqdmxuih.supabase.co';  // Coloque sua URL do Supabase aqui
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0a29udmtjbHZtZ2NxZG14dWloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNTU0NzcsImV4cCI6MjA1NjkzMTQ3N30.PbZ3S-3Fftb9kz-Nt0wBU3PaJ4Q_HBFsZhxXNABMetc';  // Coloque sua chave anon aqui
-
-// Criação do cliente Supabase
+const SUPABASE_URL = "https://dtkonvkclvmgcqdmxuih.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0a29udmtjbHZtZ2NxZG14dWloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNTU0NzcsImV4cCI6MjA1NjkzMTQ3N30.PbZ3S-3Fftb9kz-Nt0wBU3PaJ4Q_HBFsZhxXNABMetc";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Função de cadastro
@@ -14,7 +12,7 @@ async function cadastrarUsuario() {
 
     // Enviar dados para o Supabase
     const { data, error } = await supabase
-        .from('participantes') // Nome da tabela criada
+        .from("participantes") // Nome da tabela criada
         .insert([{ nome, email, whatsapp, data_cadastro }]);
 
     if (error) {
@@ -27,7 +25,7 @@ async function cadastrarUsuario() {
 }
 
 // Impede o envio do formulário e chama a função de cadastro
-document.getElementById("cadastroForm").addEventListener("submit", async function(e) {
+document.getElementById("cadastroForm").addEventListener("submit", async function (e) {
     e.preventDefault(); // Previne o envio do formulário
     await cadastrarUsuario(); // Aguarda o cadastro ser finalizado antes de prosseguir
 });
