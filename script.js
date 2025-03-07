9db8b63e35a16d0105595b21316b072cd57673cb
universoonlinesorteiogratis <andreiaerayssa@gmail.com>
2025-03-07 17:39:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuração do Supabase
 const SUPABASE_URL = 'https://dtkonvkclvmgcqdmxuih.supabase.co';  // Coloque sua URL do Supabase aqui
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0a29udmtjbHZtZ2NxZG14dWloIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTM1NTQ3NywiZXhwIjoyMDU2OTMxNDc3fQ.MBzmozKq-fTa3fziU2rMINDZX9kMdPAU0pPmRgkR2J4';  // Coloque sua chave anon aqui
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0a29udmtjbHZtZ2NxZG14dWloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNTU0NzcsImV4cCI6MjA1NjkzMTQ3N30.PbZ3S-3Fftb9kz-Nt0wBU3PaJ4Q_HBFsZhxXNABMetc';  // Coloque sua chave anon aqui
 
 // Criação do cliente Supabase
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
